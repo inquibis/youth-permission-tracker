273812cae0b8491ce5fcc094b68c12e9d16b61a7
Luke Todd <toddluke95@gmail.com>
2025-07-30 13:51:17 -0600

    test

---
+++ test_web_walkthrough_reg.py
@@ -0,0 +1,54 @@
+import pytest
+import requests
+import subprocess
+
+base_url = "http://127.0.0.1:5500"
+
+# setup
+# start api
+# start web server
+
+##############
+# initial load
+##############
+# create users:  class president, class members
+url = ""
+
+
+url = f"{base_url}/website/user-management2.html"
+
+
+################
+# activity prep
+################
+# class members
+url = f"{base_url}/website/interest-survey.html"
+
+
+# presidency
+url = f"{base_url}/website/identified-needs.html"
+
+
+url = f"{base_url}/website/view-interest-survey.html"
+
+################
+# create activity
+################
+url = "http://127.0.0.1:5500/website/submit-activity-information.html"
+
+
+###############
+# Enable activity
+###############
+url = "http://127.0.0.1:5500/website/activity-list.html"
+
+url = "http://127.0.0.1:5500/website/parental-permission.html"
+url = "http://127.0.0.1:5500/website/parental-permission2.html"
+
+url = "http://127.0.0.1:5500/website/activity-permission-status.html"
+
+
+#############
+# activity close out
+############
+url = "http://127.0.0.1:5500/website/activity-review.html"
