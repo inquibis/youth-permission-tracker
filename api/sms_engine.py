509ddd90e002d96a617e146426b26e50ff8c874e
Luke Todd <toddluke95@gmail.com>
2025-09-30 07:35:28 -0600

    update sms engine

--- api/sms_engine.py
+++ api/sms_engine.py
@@ -9,11 +9,13 @@ class SMS_Engine:
     account_sid = os.getenv('TWILIO_ACCOUNT_SID')
     auth_token = os.getenv('TWILIO_AUTH_TOKEN')
     from_number = os.getenv('TWILIO_PHONE_NUMBER')
+    sms_sid = os.getenv("TWILLIO_MESSAGE_SID")
 
 
     def send_sms(self, message:str, recipient:str)->None:
         client = Client(self.account_sid, self.auth_token)
         message = client.messages.create(
+            messaging_service_sid=self.sms_sid,
             body=message,
             from_=recipient,
             to=recipient
