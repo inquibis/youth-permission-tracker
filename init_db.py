e9feffba01a76d2cc851d1ee9a66652f43bb98fa
Luke Todd <toddluke95@gmail.com>
2026-01-25 12:22:34 -0700

    cleanup

--- init_db.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-import sqlite3
-
-load_dotenv()
-if os.getenv("ENV") == "test":
-    conn = sqlite3.connect("./activitydb.db")
-    conn.execute("CREATE TABLE IF NOT EXISTS dummy (id INTEGER PRIMARY KEY)")
-    conn.close()
